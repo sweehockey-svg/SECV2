0ad7d5e508799a221cd062204d7f63a39895e40b
sweehockey-svg <sweehockey@gmail.com>
2026-04-21 01:54:34 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.SEC_CONFIG = {
-  sheetUrl: "",
+  sheetUrl: "https://script.googleusercontent.com/macros/echo?user_content_key=AWDtjMWJKpeNAthFbynUsCr5YB-MeSWT9sfjtCX-9OEc_cdyb37-uiCtlUkH346hapFawoYbLiBAS_gAawSoou770LFB98Ib3n8mLoDEd_dclf2ZMy1Slvu5ivmN-PRIpjszw2qn_VtGj0oGpN0uKND_lfCILFWNHfMX1iKKPKaZSnvU5v_2hsc2hKjNkPbahwlRbyNdgepoEm4q42DeAwqA_US0cR3BtXk7--YNztOBeamH4zQnLTOa50T38wOHnW9rN1meP1JRUngJ5nhmu8TDZMkU2MZUcQ&lib=MSHkqhwxPXUPDWePpXNRSl5dKX0wmz4du",
   databaseUrl: ""
 };
